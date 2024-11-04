e40c5417c3e5c9f131b5d546b90fdac727265d92
txuli <asieralberdilopez@gmail.com>
2024-11-04 22:18:23 +0100

    sopnsors changes, common rendering update

--- src/app/components/sponsors.tsx
+++ src/app/components/sponsors.tsx
@@ -1,46 +1,86 @@
-"use client"
+"use client";
 import { usePathname } from 'next/navigation';
 import MainSponsor from './mainSponsor';
 import Secondsponsor from './secondSponsor';
 import Helpers from './helpers';
 
+const SPONSORS = [
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/CafeDromedario.webp', link: 'https://cafedromedario.com/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/flotamet.png', link: '' },
+];
+
+const SECONDARY_SPONSORS = [
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/vcg.png', link: 'https://www.vcg-decoletaje.com/index.html' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/betsaide.png', link: 'https://www.betsaide.com/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/tecoel.png', link: 'https://tecoel.es/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/maristak.png', link: '' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/malda.png', link: 'https://www.maldabikes.com/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/ingenia.png', link: 'https://ingenia2014.com/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/mugarra.png', link: '' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/ruralkutxa.png', link: '' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/cubical.png', link: 'https://cubicaldenira.com/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/marraz.png', link: '' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/esukadiLowCost.png', link: '' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/controlpack.png', link: 'https://www.controlpack.com/' },
+];
+
+const HELPERS = [
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/durango.png', link: 'https://www.durango.eus/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/abadiño.png', link: 'https://www.xn--abadio-0wa.org/' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/elorrio.png', link: 'https://www.elorrio.eus/eu-ES/Orriak/default.aspx' },
+    { img: 'https://photos.txuli.com/duranguesa/patrocinadores/iurreta.png', link: 'https://www.iurreta.eus/eu-ES/Orriak/default.aspx' },
+];
+
 const Sponsor = () => {
     const pathname = usePathname();
-    const isCafeDromedarioRoute = pathname === '/cafedromedario';
+
+    const renderMainSponsors = () => (
+        <>
+            <h3 className='text-5xl text-center'>BABESLE NAGUSIAK</h3>
+            <div className="grid w-10/12 mx-auto sm:grid-cols-1 lg:grid-cols-2">
+                {SPONSORS.map(({ img, link }, idx) => (
+                    <MainSponsor key={idx} img={img} Link={link} />
+                ))}
+            </div>
+        </>
+    );
+
+    const renderSecondarySponsors = () => (
+        <>
+            <h3 className='text-5xl text-center'>BABESLEAK</h3>
+            <div className="grid w-5/6 mx-auto sm:grid-cols-3 lg:grid-cols-4">
+                {SECONDARY_SPONSORS.map(({ img, link }, idx) => (
+                    <Secondsponsor key={idx} img={img} Link={link} />
+                ))}
+            </div>
+        </>
+    );
+
+    const renderHelpers = () => (
+        <>
+            <h3 className='text-center text-5xl'>LAGUNTZAILEAK</h3>
+            <div className='flex justify-center'>
+                {HELPERS.map(({ img, link }, idx) => (
+                    <Helpers key={idx} img={img} Link={link} />
+                ))}
+            </div>
+        </>
+    );
 
     return (
         <div className='w-full'>
-            <h3 className='text-5xl text-center'>BABESLE NAGUSIAK</h3>
-            <div className="grid w-10/12 mx-auto sm:grid-cols-1 lg:grid-cols-2">
-                <MainSponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/CafeDromedario.webp'} Link={"https://cafedromedario.com/"}/>
-                <MainSponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/flotamet.png'} Link={""}/>
-            </div>
-
-            {!isCafeDromedarioRoute && (
+            {pathname === '/cafedromedario' && renderMainSponsors()}
+            {pathname === '/eskola' && (
                 <>
-                    <h3 className='text-5xl text-center'>BABESLEAK</h3>
-                    <div className="grid w-5/6 mx-auto sm:grid-cols-3 lg:grid-cols-4">
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/vcg.png'} Link={"https://www.vcg-decoletaje.com/index.html"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/betsaide.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/tecoel.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/maristak.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/malda.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/ingenia.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/mugarra.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/ruralkutxa.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/cubical.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/marraz.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/esukadiLowCost.png'} Link={"https://www.betsaide.com/"}/>
-                        <Secondsponsor img={'https://photos.txuli.com/duranguesa/patrocinadores/controlpack.png'} Link={"https://www.betsaide.com/"}/>
-                    </div>
-
-                    <h3 className='text-center text-5xl'>LAGUNTZAILEAK</h3>
-                    <div className='flex justify-center'>
-                        <Helpers img={'https://photos.txuli.com/duranguesa/patrocinadores/durango.png'} Link={"https://www.betsaide.com/"}/>
-                        <Helpers img={'https://photos.txuli.com/duranguesa/patrocinadores/abadiño.png'} Link={"https://www.betsaide.com/"}/>
-                        <Helpers img={'https://photos.txuli.com/duranguesa/patrocinadores/elorrio.png'} Link={"https://www.betsaide.com/"}/>
-                        <Helpers img={'https://photos.txuli.com/duranguesa/patrocinadores/iurreta.png'} Link={"https://www.betsaide.com/"}/>
-                    </div>
+                    {renderSecondarySponsors()}
+                    {renderHelpers()}
+                </>
+            )}
+            {pathname !== '/cafedromedario' && pathname !== '/eskola' && (
+                <>
+                    {renderMainSponsors()}
+                    {renderSecondarySponsors()}
+                    {renderHelpers()}
                 </>
             )}
         </div>
