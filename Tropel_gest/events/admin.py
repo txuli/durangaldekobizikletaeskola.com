0671c10d1e7bd42aa93a906ca352f8142e9c1d3c
txuli <asierlaberdilopez@gmail.com>
2024-12-13 21:07:23 +0000

    remove tropel

--- Tropel_gest/events/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Evento, Deportista, DatosDeportista, Resultado, Categoria, Modalidad
-
-admin.site.register(Evento)
-admin.site.register(Deportista)
-admin.site.register(DatosDeportista)
-admin.site.register(Resultado)
-admin.site.register(Categoria)
-admin.site.register(Modalidad)
