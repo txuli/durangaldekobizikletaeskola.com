092f00b90da91b81b94e01edb62d55c253390708
txuli <asieralberdilopez@gmail.com>
2025-02-26 16:02:30 +0000

    update url

--- src/app/[locale]/(main)/galeria/page.tsx
+++ src/app/[locale]/(main)/galeria/page.tsx
@@ -1,30 +1,30 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Page() {
-  const [responseMessage, setResponseMessage] = useState("");
-  const [data, setData] = useState(null); // Nuevo estado para guardar los datos
+  // const [responseMessage, setResponseMessage] = useState("");
+  // const [data, setData] = useState(null); // Nuevo estado para guardar los datos
 
-  const test = async () => {
-    const response = await fetch("/api/gallery", {
-      method: "POST",
-    });
+  // const test = async () => {
+  //   const response = await fetch("/api/gallery", {
+  //     method: "POST",
+  //   });
 
-    if (response.ok) {
-      const responseData = await response.json(); // Obtén los datos de la respuesta
-      setData(responseData); // Guarda los datos en el estado
+  //   if (response.ok) {
+  //     const responseData = await response.json(); // Obtén los datos de la respuesta
+  //     setData(responseData); // Guarda los datos en el estado
       
-      setResponseMessage("Fetching correcto");
-    } else {
-      setResponseMessage("Fetching incorrecto");
-    }
+  //     setResponseMessage("Fetching correcto");
+  //   } else {
+  //     setResponseMessage("Fetching incorrecto");
+  //   }
 
-    console.log(data);
-  };
+  //   console.log(data);
+  // };
 
   return (
     <div className="mt-20">
-      <button onClick={test}>Fetch Data</button>
+      {/* <button onClick={test}>Fetch Data</button> */}
      
     </div>
   );
