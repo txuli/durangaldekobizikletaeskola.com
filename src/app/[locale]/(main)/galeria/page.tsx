c7c3e1e0d49e95b8487834136aa21d0416cb17e2
txuli <127785824+txuli@users.noreply.github.com>
2025-02-27 12:17:27 +0000

    dynamic rendering of the albums

--- src/app/[locale]/(main)/galeria/page.tsx
+++ src/app/[locale]/(main)/galeria/page.tsx
@@ -1,31 +1,54 @@
-"use client";
-// import { useState } from "react";
+"use client"; 
+import { useState, useEffect } from "react";
+import Card from "../components/gallery/Card";
 
 export default function Page() {
-  // const [responseMessage, setResponseMessage] = useState("");
-  // const [data, setData] = useState(null); // Nuevo estado para guardar los datos
+  const [responseMessage, setResponseMessage] = useState("");
+  const [data, setData] = useState(null); // Nuevo estado para guardar los datos
+  const [fetchData, setFetchData] = useState<{ image: string; title: string; }[]>([]);
 
-  // const test = async () => {
-  //   const response = await fetch("/api/gallery", {
-  //     method: "POST",
-  //   });
+  // Definir la función 'test'
+  const test = async () => {
+    const fetchData = [];
+    const response = await fetch("/api/gallery", {
+      method: "POST",
+    });
 
-  //   if (response.ok) {
-  //     const responseData = await response.json(); // Obtén los datos de la respuesta
-  //     setData(responseData); // Guarda los datos en el estado
-      
-  //     setResponseMessage("Fetching correcto");
-  //   } else {
-  //     setResponseMessage("Fetching incorrecto");
-  //   }
+    let responseData = null;
+    if (response.ok) {
+      responseData = await response.json(); // Obtén los datos de la respuesta
+      setData(responseData); // Guarda los datos en el estado
 
-  //   console.log(data);
-  // };
+      setResponseMessage("Fetching correcto");
+    } else {
+      setResponseMessage("Fetching incorrecto");
+    }
+
+    console.log(data);
+    if (responseData !== null) {
+      for (let i = 0; i < responseData.length; i++) {
+        if (!responseData[i].includes("../")) {
+          fetchData.push({
+            title: responseData[i].slice(0, -1),
+            image: "https://photos.txuli.com/duranguesa/covers/" +
+              responseData[i].slice(0, -1) + ".jpg"
+          });
+        }
+      }
+      setFetchData(fetchData);
+      console.log("Fetch Data:");
+      console.log(fetchData);
+    }
+  };
+
+  
+  useEffect(() => {
+    test();
+  }, []); 
 
   return (
     <div className="mt-20">
-      {/* <button onClick={test}>Fetch Data</button> */}
-     
+      <Card album={fetchData} />
     </div>
   );
 }
