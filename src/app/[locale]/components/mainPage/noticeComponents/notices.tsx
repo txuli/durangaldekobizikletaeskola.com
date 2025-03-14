d5abcabce15e1464c51a028818f3511c1873a43e
txuli <asieralberdilopez@gmail.com>
2025-03-14 10:57:39 +0000

    visual bug fixed

--- src/app/[locale]/components/mainPage/noticeComponents/notices.tsx
+++ src/app/[locale]/components/mainPage/noticeComponents/notices.tsx
@@ -17,11 +17,11 @@ interface NewsProps {
 
 export default function News({ items }: NewsProps) {
   return (
-    <Section className="flex flex-wrap gap-8 justify-center sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  h-116">
+    <Section className="flex flex-wrap gap-8 justify-center sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  xl:h-116">
       {items.map((item, index) => (
         <article
           key={index}  
-          className="group relative h-96 w-86 hover:w-100 lg:hover:h-100 transition-all duration-300 ease-in-out  mx-auto "
+          className="group relative h-96 w-86 hover:w-100 lg:hover:h-100 transition-all duration-300 ease-in-out  mx-auto mb-20 xl:MB-0 "
         >
           <Link href={item.href} className="hover:cursor-pointer">
             <Image
