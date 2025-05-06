d5430b9b69586348a07b5b54a5aef09de359856c
txuli <127785824+txuli@users.noreply.github.com>
2025-05-06 12:03:57 +0000

    fix

--- src/app/[locale]/(main)/notices/page.tsx
+++ src/app/[locale]/(main)/notices/page.tsx
@@ -1,53 +1,54 @@
-import {getLocale, getTranslations} from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 import News from "@/app/[locale]/components/mainPage/noticeComponents/notices";
 import Title from "@/app/[locale]/components/mainPage/Titles/Title";
-import  Section  from "@/app/[locale]/components/main/Section";
+import Section from "@/app/[locale]/components/main/Section";
 
 interface Notice {
-    href: string;
-    imageSrc: string;
-    alt: string;
-    date: string;
-    title: string;
-    category: string;
+  href: string;
+  imageSrc: string;
+  alt: string;
+  date: string;
+  title: string;
+  category: string;
+}
+interface NoticeResponse {
+  data: Notice[];
+}
+
+async function fetchNotices(locale: string): Promise<NoticeResponse> {
+  const API_URL =
+    process.env.NODE_ENV === "development"
+      ? process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT
+      : process.env.NEXT_PUBLIC_API_URL_PRODUCTION;
+
+  if (!API_URL) {
+    throw new Error("API URL no definida en las variables de entorno");
   }
-  interface NoticeResponse {
-    data: Notice[];
+
+  const response = await fetch(`${API_URL}/api/mainNotices`, {
+    method: "POST",
+    cache: "no-store",
+    body: JSON.stringify({ path: "allNotices", loc: locale }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching notices");
   }
-  
-  async function fetchNotices(locale: string): Promise<NoticeResponse> {
-    const API_URL =
-      process.env.NODE_ENV === "development"
-        ? process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT
-        : process.env.NEXT_PUBLIC_API_URL_PRODUCTION;
-  
-    if (!API_URL) {
-      throw new Error("API URL no definida en las variables de entorno");
-    }
-  
-    const response = await fetch(`${API_URL}/api/mainNotices`, {
-      method: "POST",
-      cache: "no-store", 
-      body: JSON.stringify({ path: "allNotices", loc:locale }),
-    });
-  
-    if (!response.ok) {
-      throw new Error("Error fetching notices");
-    }
-  
-    return response.json();
-   
-  }
+
+  return response.json();
+}
+
 export default async function Page() {
-    const locale = await getLocale(); 
-    const response = await fetchNotices(locale);
-    const t = await getTranslations("noticePage");
-    return (
-        <div>
-            <Title title={t("title")} />
-            <Section>
-            <News items={response.data} />
-            </Section>
-        </div>
-    )
+  const locale = await getLocale();
+  const t = await getTranslations("noticePage");
+  const response = await fetchNotices(locale);
+
+  return (
+    <div>
+      <Title title={t("title")} />
+      <Section>
+        <News items={response.data} />
+      </Section>
+    </div>
+  );
 }
