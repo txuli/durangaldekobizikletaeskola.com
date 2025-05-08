e59af9f940e89e835c67ec23d89beda87caa9d28
txuli <127785824+txuli@users.noreply.github.com>
2025-05-08 12:38:49 +0000

    Translation fixes and UI improvements: - Changed "AROPA" to "ARROPA" in eus.json. - Adjustments to the layout and NavBar to improve accessibility and user experience. - Modifications to the login form to handle errors and display success messages. - Updated styles in title and slide components. - Added package.json file for the Prisma client.

--- src/app/[locale]/components/mainPage/Titles/Title.tsx
+++ src/app/[locale]/components/mainPage/Titles/Title.tsx
@@ -6,7 +6,7 @@ export default function SubTitle({ title }: TitleProps) {
     return (
         
         <div className='w-full bg-gradient-to-r bg-customblue mb-10 py-1'>
-        <h3 className='text-4xl md:text-5xl  text-gray-200 w-full text-center font-fredoka font-semibold uppercase'>{title}</h3>
+        <h3 className='text-5xl sm:text-5xl  text-gray-200 w-full text-center font-fredoka font-semibold uppercase'>{title}</h3>
     </div>
     );
 }
