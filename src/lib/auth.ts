2d7afdb01bee0f4db44feae287a11970715eb21f
Lordlichi2006 <Lordlichi2006@gmail.com>
2025-05-08 11:29:04 +0000

    Refresh .gitignore and remove ignored files from tracking

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import { betterAuth } from "better-auth";
+import { createPool } from "mysql2/promise";
+import { admin } from "better-auth/plugins"
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const auth = betterAuth({
+    
+    trustedOrigins: [
+        'https://durangaldekobizikletaeskola.com',
+        'http://localhost:4225',
+        
+    ],
+    database: createPool({
+        host: process.env.DB_HOST || '',
+        user: process.env.DB_USER || '',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || '',
+       
+    }),
+    emailAndPassword: {    
+        enabled: true
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7, 
+        updateAge: 60 * 60 * 24 
+    },
+    plugins: [
+        admin() 
+    ]
+   
+});
+
+export default auth;
+
