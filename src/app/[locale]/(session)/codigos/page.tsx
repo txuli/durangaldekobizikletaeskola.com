100ecb8f734bd6422706787b315d4b2f5265954a
txuli <asieralberdilopez@gmail.com>
2025-05-20 07:35:43 +0000

    Merge branch 'master' of https://github.com/By-Tx/durangaldekobizikletaeskola.com

--- src/app/[locale]/(session)/codigos/page.tsx
+++ src/app/[locale]/(session)/codigos/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
+import prisma from "@/lib/prisma";
 import CodigosClient from "@/app/[locale]/components/session/dashboard/Codigos";
 
 export default async function Codigos() {
@@ -11,6 +12,35 @@ export default async function Codigos() {
     if (!session) {
         redirect("/");
     }
-    
-    return <CodigosClient />;
+
+    type Codigos = {
+        id: number;
+        code: string;
+        role: string | null;
+        expires_at: Date | null;
+        usos: number | null;
+    };
+
+    let data: Codigos[] = [];
+    const now = new Date();
+
+    data = await prisma.activation_codes.findMany({
+        where: {
+            OR: [
+                { expires_at: null },
+                { expires_at: { gt: now } }
+            ]
+        },
+        orderBy: { expires_at: "desc" }
+    });
+
+    const codigos = data.map((codigo) => ({
+        id: codigo.id,
+        code: codigo.code,
+        role: codigo.role || "",
+        expires_at: codigo.expires_at ? codigo.expires_at.toISOString() : "",
+        usos: Number(codigo.usos) || 0,
+    }));
+
+    return <CodigosClient codigos={codigos} />;
 }
