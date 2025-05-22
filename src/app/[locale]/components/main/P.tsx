093bc3b76603a619c37d81a20104408a6349c01b
Lordlichi2006 <Lordlichi2006@gmail.com>
2025-05-22 07:32:11 +0000

    Adjusted Cafedromedario yet again, changed <P> prop to be responsive, small edit to mouse pointer on ImageSubmit

--- src/app/[locale]/components/main/P.tsx
+++ src/app/[locale]/components/main/P.tsx
@@ -7,7 +7,7 @@ interface pProps {
 
 const p: React.FC<pProps> = ({ children, className = "" }) => {
   return (
-    <p className={`text-justify font-fredoka text-3xl px-8 font-light mx-8  mb-6 ${className}`}>
+    <p className={`text-justify font-fredoka xl:text-3xl lg:text-2xl md:text-xl sm:text-base xl:px-8 md:px-4 px-3 font-light xl:mx-8 md:mx-4 mx-3 mb-6 ${className}`}>
       {children}
     </p>
   );
